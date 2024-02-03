f5a72bf74e8f0b4b2144590c059ca866fd80541d
Allyza Marie Canoy <67453555+alymrcany@users.noreply.github.com>
2024-02-03 19:24:27 +0800

    Samples

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your name: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string name = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+Console.WriteLine("Hello "+name);
---
+++ maths/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+double num1, num2, result;
+
+
+Console.Write("Enter num1: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter num2: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+
+result = num1 + num2;
+Console.WriteLine("Sum of two numbers: " + result);
+
+
+
